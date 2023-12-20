a740166c283583e071384584a8c81907e7ac8e28
lenrek88 <lenrek88@yandex.ru>
2023-12-20 16:38:06 +0300

    add Weather

---
+++ Weather/localstorage.js
@@ -0,0 +1,21 @@
+
+export const storage = {
+
+    saveFavoriteCities(city) {
+        localStorage.setItem('FavoriteCity', JSON.stringify([...city]));
+    },
+
+    getFavoriteCities() {
+        return JSON.parse(localStorage.getItem('FavoriteCity'));
+    },
+    saveCurrentCity(city) {
+        localStorage.setItem('ThisCity', city);
+    },
+    getCurrentCity() {
+       return localStorage.getItem('ThisCity');
+    }
+
+
+}
+
+
