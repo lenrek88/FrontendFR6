542c96667c8b121412ac125194956b06666067d2
lenrek88 <lenrek88@yandex.ru>
2024-05-29 07:02:48 +0300

    hello, vue

---
+++ Модуль_6/22Тесты/25Vue/sctipt.vue
@@ -0,0 +1,43 @@
+<!-- скрипт -->
+<script setup>
+import { ref, watch } from "vue";
+const text = ref('');
+const gender = ref('');
+
+const serverUrl = 'https://api.genderize.io';
+// const URL = `${serverUrl}?name=${text}`;
+
+
+watch(text, (URL) => {
+    let URL = `${serverUrl}?name=${text}`; 
+})
+
+
+fetch(URL)
+        .then(promise => promise.json())
+        .then(result => {
+            if (result.gender === 'male') {
+                gender.value = 'Мужской'
+            } else {
+                gender.value = 'Женский'
+            }
+        })
+    
+</script>
+
+<!-- шаблон -->
+<template>
+      <div>
+        
+          <form><input v-model="text"></form>
+          <p>Ваш пол: {{gender}}</p>
+          <p>{{ text }}</p>
+      </div>
+</template>
+
+<!-- стили -->
+<style scoped>
+button {
+  font-weight: bold;
+}
+</style>
